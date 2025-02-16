ce2398bf255a194cc10b85584e70c137ffef19dc
Daniel Andrés Herrera Rodríguez <admin@digammastudio.com>
2025-02-16 11:54:58 -0500

    fix: remove redirect

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/login',
-        permanent: false,
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
